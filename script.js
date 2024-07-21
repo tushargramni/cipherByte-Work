e3fca4a74e6a7c76622f5eb526b4f08862387f5b
TusharGramni <113756375+TusharGramni@users.noreply.github.com>
2024-07-21 21:56:12 +0530

    All new Changes

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const taskBox = document.getElementById("input-box");
-const taskListCont = document.getElementById("list-container");
+const taskBox = document.querySelector(".input-box");
+const taskListCont = document.querySelector(".list-container");
 
 let addTask = () => {
   if (taskBox.value === "") {
     alert("You must Write Something");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = taskBox.value;
-    taskListCont.appendChild(li);
+    let tasks = document.createElement("li");
+    tasks.innerHTML = taskBox.value;
+    taskListCont.appendChild(tasks);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    tasks.appendChild(span);
   }
+  taskBox.value = "";
 };
+
+taskListCont.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+let saveData = () => {
+  localStorage.setItem("data", taskListCont.innerHTML);
+};
+let showTask = () => {
+  taskListCont.innerHTML = localStorage.getItem("data");
+};
+showTask();
