762868a97263c70aa745c7aaace6e4713cfe6246
TusharGramni <113756375+TusharGramni@users.noreply.github.com>
2024-07-31 01:07:33 +0530

    New Images added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome To EmporifyX.</h1>
+      <p>
+        EmporifyX is an ambitious project aimed at developing a robust and
+        user-friendly e-commerce website that caters to both merchants and
+        customers. This platform will provide a seamless online shopping
+        experience, empowering merchants to showcase and sell their products
+        while offering customers a convenient and secure way to browse,
+        purchase, and track their orders.
+      </p>
+    </div>
+  );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Header from "./Header";
+import shoppingCart from "../Images/shoppingCart/cart.png";
+import Cloth1 from "../Images/Clothes/Cloth1.avif";
+import Cloth2 from "../Images/Clothes/Cloth2.avif";
+import Cloth3 from "../Images/Clothes/Cloth3.avif";
+import Cloth4 from "../Images/Clothes/Cloth4.avif";
+import Cloth5 from "../Images/Clothes/Cloth5.avif";
+import Cloth6 from "../Images/Clothes/Cloth6.avif";
+import Cloth7 from "../Images/Clothes/Cloth7.avif";
+import Cloth8 from "../Images/Clothes/Cloth8.avif";
+import Cloth9 from "../Images/Clothes/Cloth9.avif";
+import Cloth10 from "../Images/Clothes/Cloth10.avif";
+import Cloth11 from "../Images/Clothes/Cloth11.avif";
+import Cloth12 from "../Images/Clothes/Cloth12.avif";
+import Cloth13 from "../Images/Clothes/Cloth13.avif";
+import Cloth14 from "../Images/Clothes/Cloth14.avif";
+// import Cloth15 from "../Images/Clothes/Cloth15.avif";
 
 export default function Main() {
   return (
-    <section classNameName="section d-flex flex-wrap p-5 ">
+    <section className="section d-flex flex-wrap p-5 gap-3 flex-grow-1">
+      <Header />
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth1} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -12,12 +30,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth2} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -25,53 +44,14 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
 
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth3} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -79,12 +59,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth4} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -92,12 +73,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth5} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -105,12 +87,28 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={Cloth6} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth7} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -118,12 +116,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth8} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -131,12 +130,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth9} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -144,12 +144,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth10} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -157,12 +158,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth11} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -170,12 +172,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth12} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -183,12 +186,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth13} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -196,12 +200,13 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Cloth14} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
@@ -209,7 +214,36 @@ export default function Main() {
             bulk of the card's content.
           </p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Cloth6} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Cloth6} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary cartbtn">
+            <img src={shoppingCart} alt="" className="cart" />
+            Add to Cart
           </a>
         </div>
       </div>
